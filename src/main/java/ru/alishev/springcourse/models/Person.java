1ce78c910498d64d1d59c4937d79bfa5625d91c6
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-03 18:52:18 +0300

    Date and time in Hibernate

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -2,12 +2,14 @@ package ru.alishev.springcourse.models;
 
 import jakarta.persistence.*;
 import org.hibernate.mapping.List;
+import org.springframework.format.annotation.DateTimeFormat;
 
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 /**
  * @author Neil Alishev
@@ -34,6 +36,16 @@ public class Person {
     @NotEmpty(message = "Email should not be empty")
     @Email
     private String email;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy") // дд/мм/гггг
+    private Date dateOfBirth;
+
+    @Column(name = "created_at")
+    @Temporal (TemporalType.TIMESTAMP)
+    private Date createdAt;
+
 
 
     @OneToMany(mappedBy = "owner")
@@ -78,6 +90,30 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 
     @Override
--- src/main/java/ru/alishev/springcourse/services/PeopleService.java
+++ src/main/java/ru/alishev/springcourse/services/PeopleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.alishev.springcourse.models.Person;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +34,7 @@ public class PeopleService {
 
     @Transactional
     public void save(Person person){
+        person.setCreatedAt(new Date());
         peopleRepository.save(person);
     }
 
